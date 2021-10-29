674208926ca1cd0fbe1ead22246c45bcb454d24a
rahulgitx <rahul18bhardwaj.23@gmail.com>
2021-10-29 11:28:42 +0530

    navbar

--- django/hello/hello/urls.py
+++ django/hello/hello/urls.py
@@ -18,6 +18,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls'), name="any")
     # path('about', include('home.ur'))
+    
 ]
+
+admin.site.site_header = "Online-judge Portal Admin"
+admin.site.site_title = "Online-judge Admin Portal"
+admin.site.index_title = "Welcome to Online-duge database Portal"
--- django/hello/home/urls.py
+++ django/hello/home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('services', views.services, name='services'),
