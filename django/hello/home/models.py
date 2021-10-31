0a002e162847368c722f5a722a619c3f74bc695d
rahulgitx <rahul18bhardwaj.23@gmail.com>
2021-10-31 15:35:59 +0530

    sunday

--- django/hello/home/models.py
+++ django/hello/home/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class Contacts(models.Model):
     email=models.CharField(max_length=122)
-    password=models.CharField(max_length=122)
+    phone=models.CharField(max_length=122)
     name=models.CharField(max_length=12)
     desc=models.TextField()
     date=models.DateField()
+
+    def __str__(self):
+        return self.name
