d8f603bbcd53db92e26c412fbebe414b73e46395
root <root@localhost.localdomain>
2021-11-16 12:28:13 +0530

    back frm library

--- django/hello/media/newfile.cpp
+++ django/hello/media/newfile.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
-	cout << "hello world new";
+	cout << "hello world new" << endl;
+	int n;
+	cin >> n;
+	for(int i=0; i<n; i++){
+		cout << i << endl;
+	}
+	int t;
+	cin >> t;
+	for(int i=0; i<n; i++){
+		cout << "printing out t: " << t << endl
+	}
 }
--- django/hello/problems/views.py
+++ django/hello/problems/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, HttpResponse
 from django.core.files.storage import FileSystemStorage
 import subprocess
 
-def runcpp():
+def runcppproblem1():
     subprocess.run('rm newfile', shell=True)
     subprocess.run('rm newfile.o', shell=True)
     subprocess.run('g++ -c /onlinejudge/Online-judge/django/hello/media/newfile.cpp', shell=True)
     subprocess.run('g++ newfile.o -o newfile', shell=True)
-    subprocess.run('./newfile > /onlinejudge/Online-judge/django/hello/media/outputfile.txt', shell=True)
+    p = subprocess.run('echo 5 3 | ./newfile > /onlinejudge/Online-judge/django/hello/media/testcase1.txt', shell=True)  #testcase 1
+    if(p.returncode != 0):
+        print('Compilation error!')
+    # subprocess.run('echo 5 1 2 3 -2 5 | ./newfile > /onlinejudge/Online-judge/django/hello/media/testcase1.txt', shell=True)  #testcase 1
+    # subprocess.run('echo 4 -1 -2 -3 -4 | ./newfile > /onlinejudge/Online-judge/django/hello/media/testcase2.txt', shell=True)  #testcase 2
     print()
     print("g++ command ran successfully")
 
@@ -23,7 +27,7 @@ def problem1(request):
         uploaded_file.name = "newfile.cpp"                       # changing the name of the incoming file
         # print(uploaded_file.name," ", uploaded_file)
         fs.save(uploaded_file.name, uploaded_file)
-        runcpp()
+        runcppproblem1()
     return render(request, 'problem1.html')
 
 def submitproblem1(request):
